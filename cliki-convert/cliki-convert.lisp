4e9bd1d499e7c551fab68565323a63326b756942
Vladimir Sedach <vsedach@gmail.com>
2011-04-05 01:14:29 -0400

    cliki-import: preserve article name case, fix sorting

--- cliki-convert/cliki-convert.lisp
+++ cliki-convert/cliki-convert.lisp
@@ -24,76 +24,69 @@
                              :input in
                              :output s)))))
 
-;;;; read-old-articles
-
-(defun read-old-articles (old-article-dir)
-  (let ((tmp (make-hash-table :test 'equal))
-        (old-articles (make-hash-table :test 'equal)))
-    
-    (dolist (file (cl-fad:list-directory old-article-dir))
-      (push file
-            (gethash (string-downcase (pathname-name file))
-                     tmp)))
-    (iter (for (title revisions) in-hashtable tmp)
-          (setf revisions
-                (sort revisions
-                      #'<
-                      :key (lambda (p)
-                               (or (ignore-errors (parse-integer (pathname-type p)))
-                                   0))))
-          (let ((last-revision (car (last revisions))))
-            (unless (search "*(delete this page)"
-                            (alexandria:read-file-into-string last-revision
-                                                              :external-format :latin1)
-                            :test #'char-equal)
-              (setf (gethash (hunchentoot:url-decode (substitute #\% #\=  (pathname-name last-revision))
-                                                     hunchentoot::+latin-1+)
-                             old-articles)
-                    revisions))))
-    old-articles))
-            
 ;;;; load-old-articles
 
 (defun load-old-articles (old-article-dir &key verbose)
   "WARNING: This WILL blow away your old store."
-  (let ((old-articles (read-old-articles old-article-dir)))
-    
-    (close-store)
-    (close-search-index)
+  (close-store)
+  (close-search-index)
 
-    (iter (for item in '("content/" "index/" "store/"))
-          (for path = (merge-pathnames item *datadir*))
-          (cl-fad:delete-directory-and-files path
-                                             :if-does-not-exist :ignore)
-          (ensure-directories-exist path))
-                        
-    (open-store (merge-pathnames "store/" *datadir*))
-    (open-search-index)
-    
+  (iter (for item in '("content/" "index/" "store/"))
+        (for path = (merge-pathnames item *datadir*))
+        (cl-fad:delete-directory-and-files path
+                                           :if-does-not-exist :ignore)
+        (ensure-directories-exist path))
+
+  (open-store (merge-pathnames "store/" *datadir*))
+  (open-search-index)
+
+  (let ((old-articles (make-hash-table :test 'equalp)) ;; equalp is case insensitive
+    (dolist (file (cl-fad:list-directory old-article-dir))
+      (let ((file-name (hunchentoot:url-decode
+                        (substitute #\% #\= (pathname-name file))
+                        hunchentoot::+latin-1+)))
+        (setf (gethash file-name old-articles)
+              (merge 'list
+                     (list file)
+                     (gethash file-name old-articles)
+                     #'< :key (lambda (x)
+                                (parse-integer (or (pathname-type x) "0")))))))
+    ;; sort revisions and discard deleted pages
+    (loop for article being the hash-key of old-articles do
+         (when (search "*(delete this page)"
+                       (alexandria:read-file-into-string
+                        (car (last (gethash article old-articles)))
+                        :external-format :latin1)
+                       :test #'char-equal)
+           (remhash article old-articles)))
     ;; import into store
     (let ((cliki-import-user (make-instance 'user
                                             :name "CLiki-importer"
                                             :email "noreply@cliki.net"
-                                            :password "nohash")))
-      (iter (for (article-title revisions) in-hashtable old-articles)
-            (for i from 0)
-            (let ((article (make-instance 'article :title article-title))
-                  (content nil)
-                  (timestamp-skew 0)) ;; needed because some revisions have identical timestamps
-              (when verbose
-                (format t
-                        "~A%; Convert ~A~%"
-                        (floor (* (/ i (hash-table-count old-articles)) 100))
-                        article-title))
-              (dolist (file revisions)
-                (add-revision article
-                              "import from CLiki"
-                              (setf content (convert-old-cliki-page file))
-                              :author cliki-import-user
-                              :author-ip "0.0.0.0"
-                              :date (+ (incf timestamp-skew) (file-write-date file))
-                              :add-to-index nil))
-              (add-article-to-index article-title content)))))
+                                            :password-salt #(0 0 0 0)
+                                            :password-digest "nohash")))
+      (loop for i from 0
+         for article-title being the hash-key of old-articles do
+           (let ((article (make-instance 'article :title article-title))
+                 (content nil)
+                 (timestamp-skew 0)) ;; needed because some revisions have identical timestamps
+             (when verbose
+               (format t
+                       "~A%; Convert ~A~%"
+                       (floor (* (/ i (hash-table-count old-articles)) 100))
+                       article-title))
+             (dolist (file (gethash article-title old-articles))
+               (add-revision article
+                             "import from CLiki"
+                             (setf content (convert-old-cliki-page file))
+                             :author cliki-import-user
+                             :author-ip "0.0.0.0"
+                             :date (+ (incf timestamp-skew) (file-write-date file))
+                             :add-to-index nil))
+             (add-article-to-index article-title content)))))
+  ;; fix up recent revisions (this will get blown away on store reload, but doesn't matter because all revisions made after import will be correct)
+  (replace *recent-revisions* (sort (store-objects-with-class 'revision) #'> :key #'revision-date))
+  (setf *recent-revisions-latest* 0)
   (bknr.datastore:snapshot))
 
 ;; (load-old-articles "/home/viper/tmp/cliki/")
